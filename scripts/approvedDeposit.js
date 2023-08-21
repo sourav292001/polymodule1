eadcd6b7ed350199e1279733c502c06ecc26165c
sourav292001 <138144029+sourav292001@users.noreply.github.com>
2023-08-21 23:47:34 +0530

    Update approvedDeposit.js

--- scripts/approvedDeposit.js
+++ scripts/approvedDeposit.js
@@ -1,63 +1,69 @@
+//import c from "contracts/examples/erc721-transfer/FxERC721RootTunnel.sol"// Import necessary packages and contracts
 const { ethers } = require("hardhat");
 const { FXRootContractAbi } = require("../artifacts/FXRootContractAbi.js");
-const { Cricketer__factory } = require("../typechain");
+const ABI = require("../artifacts/contracts/cricketer.sol/cricketer.json");
 require("dotenv").config();
 
-async function transferTokensToFxChain() {
-  try {
-    // Set up connections to network and wallet
-    const network = "goerli"; // Change this to the desired network
-    const privateKey = process.env.PRIVATE_KEY;
-    const provider = new ethers.providers.JsonRpcProvider(
-      `https://${network}.infura.io/v3/your-infura-project-id`
-    );
+//Transfer ERC721A tokens to the Ethereum FxChain network
+async function main() {
+  // Set up connections to network and wallet
+  const networkAddress =
+    "https://eth-goerli.g.alchemy.com/v2/h-asDNMJ21mVniDAY3XE1VQ9F7PW7A0x";
+  const privateKey = process.env.PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
-    // Create a wallet instance
-    const wallet = new ethers.Wallet(privateKey, provider);
+  // Create a wallet instance
+  const wallet = new ethers.Wallet(privateKey, provider);
 
-    // Get the signer instance
-    const signer = wallet.connect(provider);
+  // Get the signer instance
+  const [signer] = await ethers.getSigners();
 
-    // Get ERC721A contract instance
-    const nftContractAddress = "0x2241bdE6016B7FFa0A74C8907a6E26101C0BA4F0";
-    const nft = Cricketer__factory.connect(nftContractAddress, signer);
+  // Get ERC721A contract instance
+  const NFT = await ethers.getContractFactory("cricketer");
+  const nft = await NFT.attach("0x59057972F553f795275bE46bdC27De38EdBd9E1e");
 
-    // Get FXRoot contract instance
-    const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
-    const fxRoot = new ethers.Contract(fxRootAddress, FXRootContractAbi, signer);
+  // Get FXRoot contract instance
+  const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
+  const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
 
-    // TokenIds to transfer
-    const tokenIds = [0, 1, 2, 3, 4];
+  // TokenIds to transfer
+  const tokenIds = [0, 1, 2, 3, 4];
 
-    // Approve the nfts for transfer
-    const approveTx = await nft.setApprovalForAll(fxRootAddress, true);
-    await approveTx.wait();
-    console.log("Approval confirmed");
+  // Approve the nfts for transfer
+  const approveTx = await nft
+    .connect(signer)
+    .setApprovalForAll(fxRootAddress, true);
+  await approveTx.wait();
+  console.log("Approval confirmed");
 
-    // Deposit the nfts to the FXRoot contract
-    for (const tokenId of tokenIds) {
-      const depositTx = await fxRoot.depositERC721(nft.address, tokenId, network);
-      await depositTx.wait();
-      console.log(`Token ${tokenId} deposited to FXChain`);
-    }
+  // Deposit the nfts to the FXRoot contracts
+  for (let i = 0; i < tokenIds; i++) {
+    const depositTx = await fxRoot
+      .connect(signer)
+      .deposit(nft.address, wallet.address, tokenIds[i], "0x6566");
 
-    console.log("Approved and deposited");
-
-    // Test balanceOf
-    const balance = await nft.balanceOf(wallet.address);
-
-    // Print the balance of the wallet
-    console.log(
-      "IndianNFT wallet balance",
-      wallet.address,
-      "is: ",
-      balance.toString()
-    );
-  } catch (error) {
-    console.error("An error occurred during the transfer:", error);
-    process.exit(1);
+    // Wait for the deposit to be confirmed
+    await depositTx.wait();
   }
+
+  console.log("Approved and deposited");
+
+  // Test balanceOf
+  const balance = await nft.balanceOf(wallet.address);
+
+  // Print the balance of the wallet
+  console.log(
+    "IndianNFT wallet balance",
+    wallet.address,
+    "is: ",
+    balance.toString()
+  );
 }
 
-// Call the transfer function
-transferTokensToFxChain();
+// Call the main function and handle any errors
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
