e82e8004c507dcf48f56eda0649c1cd008a06432
sourav292001 <138144029+sourav292001@users.noreply.github.com>
2023-08-21 23:46:12 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,32 @@
 const hre = require("hardhat");
 const fs = require("fs");
 
-async function deployContractAndExportAddress() {
-  try {
-    // Get the contract factory
-    const NFT = await hre.ethers.getContractFactory("cricketer");
+async function main() {
+  // Get the contract factory
+  const NFT = await hre.ethers.getContractFactory("cricketer");
 
-    // Deploy the contract
-    const nft = await NFT.deploy();
+  // Deploy the contract
+  const nft = await NFT.deploy();
 
-    // Wait for the contract to be deployed
-    await nft.deployed();
+  // Wait for the contract to be deployed
+  await nft.deployed();
 
-    // Log the contract address
-    console.log("NFT contract deployed to:", nft.address);
+  // Log the contract address
+  console.log("NFT contract deployed to:", nft.address);
 
-    // Export the contract address to a JavaScript file
-    const contractAddressJs = `
-      export const nftAddress = "${nft.address}";
-    `;
-
-    fs.writeFileSync("metadata/contractAddress.js", contractAddressJs);
-
-    console.log("Contract address exported to metadata/contractAddress.js");
-  } catch (error) {
-    console.error("An error occurred during deployment:", error);
-    process.exit(1);
-  }
+  // export the addresses
+  fs.writeFileSync(
+    "metadata/contractAddress.js",
+    `
+    export const nftAddress = "${nft.address}"
+  `
+  );
 }
 
-// Execute the deployment and export function
-deployContractAndExportAddress();
+// Execute the deployment function
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
